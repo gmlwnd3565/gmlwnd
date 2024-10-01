11496c36b40fd14d8e867530f6113c541ef811d1
tjdgus9955 <tjdgus9955@gmail.com>
2024-10-01 18:09:49 +0900

    v9.2

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -9,7 +9,21 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
   }
 }
 
+resource "aws_db_parameter_group" "utf8mb4" {
+  name        = "my-utf8mb4-parameter-group"
+  family      = "mysql8.0"
+  description = "Parameter group for utf8mb4"
 
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_unicode_ci"
+  }
+}
 
 resource "aws_db_instance" "rds" {
   allocated_storage    = 20
@@ -21,6 +35,7 @@ resource "aws_db_instance" "rds" {
   username             = var.username
   password             = var.password
   publicly_accessible  = true
+  parameter_group_name   = aws_db_parameter_group.utf8mb4.name
   identifier             = var.instance_identifier
   vpc_security_group_ids = [var.security_group_id]
   skip_final_snapshot = true
